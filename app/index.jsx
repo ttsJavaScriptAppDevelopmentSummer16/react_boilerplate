ff1af83860ebbc035c128f5ff57371123a0ae0b4
Priyam Sarma <priyam.sarma@gmail.com>
2016-09-07 03:50:35 +0000

    Adds file after solution to first class Exercise

--- app/index.jsx
+++ app/index.jsx
@@ -1,1 +1,16 @@
-document.getElementById('root').innerHTML = "<h1> Hello World!</h1>"
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class MessageInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type = 'text' value = 'Insert Text'/>
+        <input type = 'button' value = 'Click Here'/>
+      </div>
+    )
+  }
+}
+
+const mountPoint = document.querySelector('#root')
+ReactDOM.render(<MessageInput/>,mountPoint)
